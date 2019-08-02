e48a517718e9b0ec8bb224008db658f1afd910ed
omda <abuoop123@gmail.com>
2019-08-02 03:49:34 +0200

    updae index at public

--- public/index.php
+++ public/index.php
@@ -38,9 +38,9 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 // set the public path to this directory
-$app->bind('path.public', function() {
-    return __DIR__;
-});
+// $app->bind('path.public', function() {
+//     return __DIR__;
+// });
 
 /*
 |--------------------------------------------------------------------------
