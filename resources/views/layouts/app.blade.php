4c60050563bd91bf9efbfee104638d89e7580ead
omda <abuoop123@gmail.com>
2019-08-02 03:13:31 +0200

    remove from path public folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>@yield('title')</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('public/js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,14 +19,14 @@
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
-    <link href="{{ asset('public/backend/css/material-dashboard.css?v=2.1.0')}}" rel="stylesheet" />
-    <link href="{{ asset('public/backend/css/material-dashboard-rtl.css?v=1.1')}}" rel="stylesheet" />
+    <link href="{{ asset('backend/css/material-dashboard.css?v=2.1.0')}}" rel="stylesheet" />
+    <link href="{{ asset('backend/css/material-dashboard-rtl.css?v=1.1')}}" rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{ asset('public/backend/demo/demo.css')}}" rel="stylesheet" />
-    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/demo/demo.css')}}" rel="stylesheet" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     @stack('css')
-    
+
   <!-- Style Just for persian demo purpose, don't include it in your project -->
   <style>
     body,
@@ -54,9 +54,9 @@
             <div class="main-panel">
                 @if(Request::is('admin*'))
                   @include('layouts.parials.topbar')
-                @endif     
+                @endif
                 <div class="content">
-                  <div class="container-fluid"> 
+                  <div class="container-fluid">
                     @yield('content')
                   </div>
                 </div>
@@ -67,27 +67,27 @@
         </div>
     </div>
 
-    
+
      <!--   Core JS Files   -->
-     <script src="{{ asset('public/js/core/jquery.min.js')}}" type="text/javascript"></script>
-     <script src="{{ asset('public/js/core/popper.min.js')}}" type="text/javascript"></script>
-     <script src="{{ asset('public/js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
-     <script src="{{ asset('public/js/plugins/perfect-scrollbar.jquery.min.js')}}"></script>
+     <script src="{{ asset('js/core/jquery.min.js')}}" type="text/javascript"></script>
+     <script src="{{ asset('js/core/popper.min.js')}}" type="text/javascript"></script>
+     <script src="{{ asset('js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
+     <script src="{{ asset('js/plugins/perfect-scrollbar.jquery.min.js')}}"></script>
      <!--  Google Maps Plugin    -->
      <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
      <!-- Chartist JS -->
-     <script src="{{ asset('public/js/plugins/chartist.min.js')}}"></script>
+     <script src="{{ asset('js/plugins/chartist.min.js')}}"></script>
      <!--  Notifications Plugin    -->
-     <script src="{{ asset('public/js/plugins/bootstrap-notify.js')}}"></script>
+     <script src="{{ asset('js/plugins/bootstrap-notify.js')}}"></script>
      <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-     <script src="{{ asset('public/js/material-dashboard.min.js?v=2.1.0')}}" type="text/javascript"></script>
+     <script src="{{ asset('js/material-dashboard.min.js?v=2.1.0')}}" type="text/javascript"></script>
      <!-- Material Dashboard DEMO methods, don't include it in your project! -->
      <script src="{{ asset('demo/demo.js')}}"></script>
      <script>
        $(document).ready(function () {
          // Javascript method's body can be found in assets/js/demos.js
          md.initDashboardPageCharts();
- 
+
        });
      </script>
 
